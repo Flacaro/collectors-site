2e4b4f775166ce2bfb3c3317d2f5b7329e78b2cc
Flacaro <flavianacaroselli@gmail.com>
2023-02-09 10:49:59 +0100

    messe le select in addDisk

--- src/app/components/diskAddDialog/dialog.component.ts
+++ src/app/components/diskAddDialog/dialog.component.ts
@@ -19,7 +19,58 @@ export class DialogComponent implements OnInit {
     {string: 'ottimo', viewValue: 'Ottimo'},
     {string: 'discreto', viewValue: 'Discreto'},
   ];
+
+
+  genre: Select[] = [
+    {string: 'rock', viewValue: 'Rock'},
+    {string: 'pop', viewValue: 'Pop'},
+    {string: 'jazz', viewValue: 'Jazz'},
+    {string: 'blues', viewValue: 'Blues'},
+    {string: 'metal', viewValue: 'Metal'},
+    {string: 'punk rock', viewValue: 'Punk Rock'},
+    {string: 'hip-hop', viewValue: 'Hip-Hop'},
+    {string: 'reggae', viewValue: 'Reggae'},
+    {string: 'country', viewValue: 'Country'},
+    {string: 'classica', viewValue: 'Classica'},
+    {string: 'soul', viewValue: 'Soul'},
+    {string: 'disco', viewValue: 'Disco'},
+    {string: 'funk', viewValue: 'Funk'},
+    {string: 'indie', viewValue: 'Indie'},
+    {string: 'electronic', viewValue: 'Electronic'},
+    {string: 'alternative', viewValue: 'Alternative'},
+    {string: 'folk', viewValue: 'Folk'},
+    {string: 'rap', viewValue: 'Rap'},
+    {string: 'ska', viewValue: 'Ska'},
+    {string: 'new wave', viewValue: 'New Wave'},
+    {string: 'hard rock', viewValue: 'Hard Rock'},
+    {string: 'heavy metal', viewValue: 'Heavy Metal'},
+    {string: 'progressive rock', viewValue: 'Progressive Rock'},
+    {string: 'psychedelic rock', viewValue: 'Psychedelic Rock'},
+  ];
   
+
+  format: Select[] = [
+    {string: 'vinile', viewValue: 'Vinile'},
+    {string: 'cd', viewValue: 'CD'},
+    {string: 'cassette', viewValue: 'Cassette'},
+    {string: 'dvd', viewValue: 'DVD'},
+    {string: 'blu-ray', viewValue: 'Blu-Ray'},
+    {string: 'vhs', viewValue: 'VHS'},
+    {string: 'k7', viewValue: 'K7'},
+    {string: 'kasetto', viewValue: 'Kasetto'},
+    {string: 'laserdisc', viewValue: 'Laserdisc'},
+    {string: 'mini disc', viewValue: 'Mini Disc'},
+    {string: 'sacd', viewValue: 'SACD'},
+    {string: 'vcd', viewValue: 'VCD'},
+    {string: 'svcd', viewValue: 'SVCD'},
+    {string: 'hd-dvd', viewValue: 'HD-DVD'},
+    {string: 'udf', viewValue: 'UDF'},
+    {string: 'dvd-audio', viewValue: 'DVD-Audio'},
+    {string: 'dvd-video', viewValue: 'DVD-Video'},
+    
+  ];
+
+
 
   constructor(
     private formBuilder: FormBuilder,
